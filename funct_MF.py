b7a74ab34996c19a6e9d7408e57eb6d9a99d1063
Amar Pohekar <amr.pohekar@gmail.com>
2022-02-04 19:51:10 +0530

    filepath_funct() and table_extract() created

--- funct_MF.py
+++ funct_MF.py
@@ -9,7 +9,8 @@ import camelot
 
 '''
 @function : filepath_funct()
-@brief    : this function used for getting file path
+@brief    : this function used for getting pdf file path
+@return   : it will give filepath of pdf file insystem directory 
 '''
 
 def filepath_funct():
@@ -17,6 +18,16 @@ def filepath_funct():
     print(filepath)
     return filepath
 
+'''
+@function : table_extract()
+@brief    : this function used to extract table from the pdf file
+@param    : filepath = filedialog.askopenfilename()
+@return   : this function import all table from pdf file and export into xlsx file format
+'''
 
-
+def table_extract(filepath):
+    table = camelot.read_pdf(filepath,pages='all')
+    table.export('sample_data.xlsx',f = 'xlsx')
+    return 0
+    
 
