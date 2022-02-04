c18194c345b559ff67a25ad6f2fb1574b5b97f56
Amar Pohekar <amr.pohekar@gmail.com>
2022-02-05 02:02:49 +0530

    main.py updated

--- main.py
+++ main.py
@@ -3,3 +3,34 @@
 @brief : this application file 
 '''
 
+import funct_MF
+
+
+filepath = ''
+
+if __name__=="__main__":
+    print('''
+          .
+          .
+          Welcome to the demo ''')
+    while True == 1:
+        option = int(input(''' Selct one of the following option
+                               1> Select and open the load TDS
+                               2> Select the pdf File
+                               3> Extract the table from pdf file
+                               4> Exit
+                               :  '''))
+        if option ==1:
+            funct_MF.openload_tds()
+        
+        elif option == 2:
+            filepath = funct_MF.filepath_funct()
+            # print(filepath)
+        
+        elif option == 3:
+            funct_MF.table_extract(filepath)
+            
+        else:
+            exit()       
+                
+            
