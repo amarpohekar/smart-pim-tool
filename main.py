d8b6d95d21a6d6651e6ca82a9826733627ec4560
Amar Pohekar <amr.pohekar@gmail.com>
2022-02-08 12:50:39 +0530

    main.py updated and modified

--- main.py
+++ main.py
@@ -16,31 +16,29 @@ if __name__=="__main__":
           Welcome to the demo ''')
     while True == 1:
         option = int(input(''' Selct one of the following option
-                               1> Select and open the load TDS pdf file
-                               2> Select the pdf File
-                               3> Extract the table from pdf file
-                               4> Select load template file
-                               5> Select the Sample load tempplate file
-                               6> Print all column header
-                               7> Exit
+                               1> Select the pdf File
+                               2> Extract the table from pdf file
+                               3> Select load template file
+                               4> print all columns header from load tempplate file
+                               5> Print all column header Sample data
+                               6> Exit
                                :-  '''))
-        if option ==1:
-            funct_MF.openload()
         
-        elif option == 2:
+        if option == 1:
             filepath = funct_MF.filepath_funct()
             print(filepath)
         
-        elif option == 3:
+        elif option == 2:
             funct_MF.table_extract(filepath)
             
+        elif option == 3:
+            load_template = funct_MF.filepath_funct()
+            print(load_template)
+        
         elif option == 4:
             funct_MF.openload()
         
         elif option == 5:
-            funct_MF.openload()
-        
-        elif option == 6:
             list_columns_header.column_header()
                 
         else:
