7d5256b0946ae6d097ead77be53b93d9f39d519f
Amar Pohekar <amr.pohekar@gmail.com>
2022-02-08 14:03:14 +0530

    list_column_header.py modified and updated

--- list_columns_header.py
+++ list_columns_header.py
@@ -3,13 +3,15 @@
 @brief : In this file have extraction of column header in list format from "sample_data.xlsx" by using pandas liabrary.
 '''
 
+from tkinter import filedialog
 import pandas as pd
 
 
-def sample_column_header():
-    df = pd.read_excel('sample_data.xlsx')
+def sample_column_header(filepath):
+    filepath = filedialog.askopenfilename()
+    df = pd.read_excel(filepath)
     list1 = list(df[1])
-    df1 = pd.read_excel('sample_data.xlsx',sheet_name=1)
+    df1 = pd.read_excel(filepath,sheet_name=1)
     df1.columns=list("ABCDEFG")
     newdf = df1.drop(["A"],axis=1)
     list2 = list(newdf.loc[0])
@@ -19,8 +21,9 @@ def sample_column_header():
     print(columns_header)
     return 0
 
-def load_template_header():
-    data = pd.read_excel('MultiConductor Load Sheet Template.xlsx')
+def load_template_header(filepath):
+    filepath = filedialog.askopenfilename()
+    data = pd.read_excel(filepath)
     columns_header = list(data.columns)
     print("column header list :- ")
     print(columns_header)
