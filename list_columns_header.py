b4ed2c0e45e367e0cacb0b7df0d04aa801315d54
Amar Pohekar <amr.pohekar@gmail.com>
2022-02-10 18:25:47 +0530

    List_column_header.py updated and modified

--- list_columns_header.py
+++ list_columns_header.py
@@ -5,6 +5,7 @@
 '''
 
 from tkinter import filedialog
+import tkinter as tk
 import pandas as pd
 
 '''
@@ -13,19 +14,24 @@ import pandas as pd
 @param    :  
 @return   : this will gives column header
 '''
-def sample_column_header(filepath):
-    filepath = filedialog.askopenfilename()
-    df = pd.read_excel(filepath)
-    list1 = list(df[1])
-    df1 = pd.read_excel(filepath,sheet_name=1)
-    df1.columns=list("ABCDEFG")
-    newdf = df1.drop(["A"],axis=1)
-    list2 = list(newdf.loc[0])
-    list1.extend(list2)
-    columns_header = list1
-    print("columns header list :- ")
-    print(columns_header)
-    return 0
+def sample_column_header():
+    try:
+        root = tk.Tk() 
+        filepath = filedialog.askopenfilename()
+        root.destroy()
+        df = pd.read_excel(filepath)
+        list1 = list(df[1])
+        df1 = pd.read_excel(filepath,sheet_name=1)
+        df1.columns=list("ABCDEFG")
+        newdf = df1.drop(["A"],axis=1)
+        list2 = list(newdf.loc[0])
+        list1.extend(list2)
+        print("columns header list :- ")
+        print(list1)
+        return list1
+    except:
+        print("Error in Execution")
+
 
 '''
 @function : load_template_header(filepath)
@@ -34,13 +40,17 @@ def sample_column_header(filepath):
 @return   : this will gives column header
 '''
 
-def load_template_header(filepath):
-    filepath = filedialog.askopenfilename()
-    data = pd.read_excel(filepath)
-    columns_header = list(data.columns)
-    print("column header list :- ")
-    print(columns_header)
-    return 0
-
+def load_template_header():
+    try:
+        root = tk.Tk()
+        filepath = filedialog.askopenfilename()
+        root.destroy()
+        data = pd.read_excel(filepath)
+        list2 = list(data.columns)
+        print("column header list :- ")
+        print(list2)
+        return list2
+    except:
+        print("Error in Execution")
 
 
